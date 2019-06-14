0d56e2594f306e5b9916dc52e8bf82d6d9d5d51c
Nur Insan <theofist02@gmail.com>
2019-06-14 14:44:49 +0800

    tes

--- DiagnosaAnak/app/src/main/java/com/example/diagnosaanak/LoginActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.diagnosaanak;
-
-public class LoginActivity {
-}
--- DiagnosaAnak/app/src/main/java/com/example/diagnosaanak/server.java
+++
@@ -1,7 +0,0 @@
-package com.example.diagnosaanak;
-
-public class server  {
-    public static final String URL = "http://192.168.1.74/projectTA/";
-}
-
-
