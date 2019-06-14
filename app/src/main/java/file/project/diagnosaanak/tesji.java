fdd2414ca8d9b5f3d04c9079bffef74156e4cc16
fadhlan <fadhlanmuhh@gmail.com>
2019-06-14 14:44:43 +0700

    file baru

---
+++ app/src/main/java/file/project/diagnosaanak/tesji.java
@@ -0,0 +1,13 @@
+package file.project.diagnosaanak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tesji extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tesji);
+    }
+}
